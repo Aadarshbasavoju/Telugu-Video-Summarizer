19adaa5a3435f7b8e707fb6a9f8baeb2fc52916d
0xAadarsh <74522137+Aadarshbasavoju@users.noreply.github.com>
2024-08-21 14:25:58 +0530

    Update translation.py

--- back-end/translation.py
+++ back-end/translation.py
@@ -1,4 +1,4 @@
-#function of translation English to Telugu 
+#function of translation English to Telugu Language
 
 def translate_to_telugu(text):
     translated = translator.translate(text, src='en', dest='te')
